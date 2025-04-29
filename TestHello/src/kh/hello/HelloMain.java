94f3b2e8b63d97eebafa40b9539e8dbb9f722aa7
HwaRang Kim <khrang54@gmail.com>
2025-04-29 17:27:07 +0900

    [Project Create] 학생성적프로그램프로젝트 시작

---
+++ TestHello/src/kh/hello/HelloMain.java
@@ -0,0 +1,10 @@
+package kh.hello;
+
+public class HelloMain {
+
+	public static void main(String[] args) {
+		System.out.println("처음으로 이클립스를 톻해서 깃허브에 연동합니다.. 흑흑흑.. 감동");
+		System.out.println("The end!");
+	}
+
+}
